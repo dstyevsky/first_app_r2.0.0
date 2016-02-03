76b50c90dcf1dba4be7cc5f65940a3368201429e
YOUR NAMEexitquit~git config --global user.name YOUR <slimones9@gmail.com>
2016-02-03 22:15:43 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstAppR200::Application.config.secret_key_base = '7b407b9e5a7b2f8311825ae7a7977955bbd410fd859a7ea46f945640664b6ce543553c3546973073020d8071d333e9370ec9d0d0e46ea3d8d7ec0ba802809b1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstAppR200::Application.config.session_store :cookie_store, key: '_first_app_r2_0_0_session'
